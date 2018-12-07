e20a859b3ef412432b2f77655939154e07f96a3b
Earl Huskey <earl@liveplanet.net>
2018-12-06 16:22:09 -0800

    Vendor updates

--- vendor/github.com/videocoin/common/vars/vars.go
+++
@@ -1,9 +0,0 @@
-package vars
-
-// Channel names
-const (
-	TranscodeProgress = "transcode-progress"
-	TranscodeStatus   = "transcode-status"
-
-	TranscodeStatusQueueGroup = "transcode-queue-group"
-)
