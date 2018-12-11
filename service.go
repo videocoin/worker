449d823b2fb82dadab20d8093af67df31b22d7dd
Earl Huskey <earl@liveplanet.net>
2018-12-10 17:10:25 -0800

    Drop audio

--- service.go
+++ service.go
@@ -194,11 +194,11 @@ func buildCmd(inputURL string, dir string) []string {
 	// p720 := fmt.Sprintf("-vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 10 -hls_playlist_type event -hls_list_size 60 -hls_wrap 10 -start_number 1 -flags -global_header -b:v 2800k -maxrate 2996k -bufsize 4200k -hls_segment_filename %s/720p_%%03d.ts %s/720p.m3u8", dir, dir)
 	// /	p1080 := fmt.Sprintf("-vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 10 -hls_playlist_type event -b:v 5000k -maxrate 5350k -bufsize 7500k -hls_segment_filename %s/1080p_%%03d.ts %s/1080p.m3u8", dir, dir)
 
-	p360 := fmt.Sprintf("-hls_flags delete_segments+append_list -f segment -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:v h264 -profile:v main -pix_fmt yuv420p -crf 20 -segment_list_flags live -segment_time 10 -segment_list_size 3 -segment_format mpegts -segment_list %s/360p/index.m3u8 %s/360p/%%03d.ts", dir, dir)
+	p360 := fmt.Sprintf("-hls_flags delete_segments+append_list -f segment -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:v h264 -profile:v main -pix_fmt yuv420p -crf 20 -segment_list_flags live -segment_time 10 -segment_list_size 3 -segment_format mpegts -an -segment_list %s/360p/index.m3u8 %s/360p/%%03d.ts", dir, dir)
 
-	p480 := fmt.Sprintf("-hls_flags delete_segments+append_list -f segment -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:v h264 -profile:v main -pix_fmt yuv420p -crf 20 -segment_list_flags live -segment_time 10 -segment_list_size 3 -segment_format mpegts -segment_list %s/480p/index.m3u8 %s/480p/%%03d.ts", dir, dir)
+	p480 := fmt.Sprintf("-hls_flags delete_segments+append_list -f segment -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:v h264 -profile:v main -pix_fmt yuv420p -crf 20 -segment_list_flags live -segment_time 10 -segment_list_size 3 -segment_format mpegts -an -segment_list %s/480p/index.m3u8 %s/480p/%%03d.ts", dir, dir)
 
-	p720 := fmt.Sprintf("-hls_flags delete_segments+append_list -f segment -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:v h264 -profile:v main -pix_fmt yuv420p -crf 20 -segment_list_flags live -segment_time 10 -segment_list_size 3 -segment_format mpegts -segment_list %s/720p/index.m3u8 %s/720p/%%03d.ts", dir, dir)
+	p720 := fmt.Sprintf("-hls_flags delete_segments+append_list -f segment -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:v h264 -profile:v main -pix_fmt yuv420p -crf 20 -segment_list_flags live -segment_time 10 -segment_list_size 3 -segment_format mpegts -an -segment_list %s/720p/index.m3u8 %s/720p/%%03d.ts", dir, dir)
 
 	cmd := []string{"-i", inputURL}
 	cmd = append(cmd, strings.Split(p360, " ")...)
