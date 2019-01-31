da5f54a575b8e614dcad99432eefdb64fd0ffe35
earl <earl@liveplanet.net>
2019-01-31 14:28:23 -0800

    Remove unused variables

--- config.go
+++ config.go
@@ -9,16 +9,12 @@ import (
 
 // Config default config for transcoder
 type Config struct {
-	LogLevel       string `required:"true" default:"debug" envconfig:"LOG_LEVEL" default:"DEBUG"`
 	BaseStreamURL  string `required:"true" envconfig:"BASE_STREAM_URL" default:"http://ingest.videocoin.io:8080/hls"`
 	VerifierURL    string `required:"true" envconfig:"VERIFIER_URL" default:"http://verifier:8100"`
 	BaseStorageURL string `required:"true" envconfig:"BASE_STORAGE_URL"`
 	Bucket         string `required:"true" envconfig:"FUSE_BUCKET"`
 	OutputDir      string `required:"true" envconfig:"OUTPUT_DIR" default:"/opt/mnt/" description:"Mount point for GCSFUSE"`
-	NATsURL        string `required:"true" envconfig:"NATS_URL" default:"nats://nats:4222"`
-	NATsToken      string `required:"true" envconfig:"NATS_TOKEN"`
 	ManagerRPCADDR string `required:"true" envconfig:"MANAGER_RPC_ADDR" default:"manager:50051"`
-	Cluster        string `required:"true" envconfig:"NATS_CLUSTER"`
 	BlockchainURL  string `required:"true" envconfig:"BLOCKCHAIN_URL" default:"http://localhost:8545"`
 	SMCA           string `required:"true" envconfig:"SMCA" default:"0xEa91ac0B88F84e91e79Caa871d2EB04eF5133721" description:"stream manager contract address"`
 	KeyFile        string `required:"true" envconfig:"KEY_FILE" default:"/opt/transcoder.key"`
