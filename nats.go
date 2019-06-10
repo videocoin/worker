5179fa07179efb54db8865c003a22763b99c8242
earl <earl@liveplanet.net>
2019-06-10 16:56:55 -0700

    Update subscribe

--- nats.go
+++ nats.go
@@ -6,29 +6,19 @@ import (
 	transcoder_v1 "github.com/VideoCoin/cloud-api/transcoder/v1"
 )
 
-var (
-	assignmentCh = make(chan *transcoder_v1.Assignment)
-)
-
 func (s *Service) subscribe(uid string) {
 	s.heartBeat(uid)
-	_, err := s.ec.BindRecvChan(uid, assignmentCh)
-	if err != nil {
-		s.log.Errorf("failed to bind recieve channel: %s", err.Error())
-	}
+
 	s.listenForAssignment(uid)
 }
 
 func (s *Service) listenForAssignment(uid string) {
-	for {
-		a := <-assignmentCh
-		s.log.Info("recieved assignment")
+	s.ec.Subscribe(uid, func(a *transcoder_v1.Assignment) {
 		err := s.handleTranscode(a.Workorder, a.Profile, uid)
 		if err != nil {
 			s.log.Errorf("failed to handle transcode: %s", err.Error())
 		}
-	}
-
+	})
 }
 
 func (s *Service) heartBeat(uid string) {
