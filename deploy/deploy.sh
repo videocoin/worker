51dea8911ac45590852999b915a86000c7eba34c
earl <earl@liveplanet.net>
2019-06-18 15:45:00 -0700

    Make staging branch [ci skip]

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ readonly CHART_NAME=transcoder
 readonly CHART_DIR=./helm/transcoder
 
 CONSUL_ADDR=${CONSUL_ADDR:=127.0.0.1:8500}
-ENV=${ENV:=thor}
+ENV=${ENV:=groot}
 DOCKER_REGISTRY=us.gcr.io
 VERSION=${VERSION:=`git describe --abbrev=0`-`git rev-parse --short HEAD`}
 PROJECT=${PROJECT:=`gcloud config list --format 'value(core.project)' 2>/dev/null`}
