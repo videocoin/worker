a91e8132fbf68cedd0dcba597781e938989c90fd
earl <earl@liveplanet.net>
2019-07-16 14:36:04 -0700

    Remove unused eth objects

--- models.go
+++ models.go
@@ -5,14 +5,11 @@ import (
 	"math/big"
 	"os/exec"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/grafov/m3u8"
 	"github.com/sirupsen/logrus"
 	manager_v1 "github.com/videocoin/cloud-api/manager/v1"
 	verifier_v1 "github.com/videocoin/cloud-api/verifier/v1"
-	"github.com/videocoin/cloud-pkg/streamManager"
 )
 
 // Byte constants
@@ -43,13 +40,10 @@ type (
 
 	// Service primary reciever for service
 	Service struct {
-		cfg           *Config
-		log           *logrus.Entry
-		ctx           context.Context
-		bcClient      *ethclient.Client
-		bcAuth        *bind.TransactOpts
-		streamManager *streamManager.Manager
-		manager       manager_v1.ManagerServiceClient
-		verifier      verifier_v1.VerifierServiceClient
+		cfg      *Config
+		log      *logrus.Entry
+		ctx      context.Context
+		manager  manager_v1.ManagerServiceClient
+		verifier verifier_v1.VerifierServiceClient
 	}
 )
