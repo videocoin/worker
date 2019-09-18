c2f5ae9d3998d8a92c4874bd409b80c657a65150
Nikita Savik <nikita@liveplanet.net>
2019-09-18 14:27:53 +0300

    Minor config refactoring

--- config.go
+++ config.go
@@ -9,12 +9,12 @@ import (
 
 // Config default config for transcoder
 type Config struct {
-	BaseStreamURL   string `required:"true" envconfig:"BASE_STREAM_URL" default:"http://ingest.videocoin.io:8080/hls"`
-	VerifierRPCADDR string `required:"true" envconfig:"VERIFIER_RPC_ADDR" default:"verifier:50055"`
+	BaseStreamURL   string `required:"true" envconfig:"BASE_STREAM_URL"`
+	VerifierRPCADDR string `required:"true" envconfig:"VERIFIER_RPC_ADDR" default:"127.0.0.1:50055"`
 	BaseStorageURL  string `required:"true" envconfig:"BASE_STORAGE_URL"`
 	Bucket          string `required:"true" envconfig:"BUCKET"`
-	OutputDir       string `required:"true" envconfig:"OUTPUT_DIR" default:"/opt/mnt/" description:"local folder for ts chunks"`
-	ManagerRPCADDR  string `required:"true" envconfig:"MANAGER_RPC_ADDR" default:"manager:50051"`
+	OutputDir       string `required:"true" envconfig:"OUTPUT_DIR" default:"/tmp" description:"local folder for ts chunks"`
+	ManagerRPCADDR  string `required:"true" envconfig:"MANAGER_RPC_ADDR" default:"127.0.0.1:50051"`
 	BlockchainURL   string `required:"true" envconfig:"BLOCKCHAIN_URL" default:"http://localhost:8545"`
 	SMCA            string `required:"true" envconfig:"SMCA" default:"0xEa91ac0B88F84e91e79Caa871d2EB04eF5133721" description:"stream manager contract address"`
 	Key             string `required:"true" envconfig:"KEY"`
