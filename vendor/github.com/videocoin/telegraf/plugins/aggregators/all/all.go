56a86054650f8ecab8d37df7e40f2cccaea2c009
Aleksandr Didenko <aa.didenko@yandex.com>
2020-01-16 18:23:43 +0300

    Remove telegraf

--- vendor/github.com/videocoin/telegraf/plugins/aggregators/all/all.go
+++
@@ -1,9 +0,0 @@
-package all
-
-import (
-	_ "github.com/videocoin/telegraf/plugins/aggregators/basicstats"
-	_ "github.com/videocoin/telegraf/plugins/aggregators/final"
-	_ "github.com/videocoin/telegraf/plugins/aggregators/histogram"
-	_ "github.com/videocoin/telegraf/plugins/aggregators/minmax"
-	_ "github.com/videocoin/telegraf/plugins/aggregators/valuecounter"
-)
--- vendor/github.com/videocoin/telegraf/plugins/inputs/all/all.go
+++
@@ -1,8 +0,0 @@
-package all
-
-import (
-	_ "github.com/videocoin/telegraf/plugins/inputs/cpu"
-	_ "github.com/videocoin/telegraf/plugins/inputs/disk"
-	_ "github.com/videocoin/telegraf/plugins/inputs/mem"
-	_ "github.com/videocoin/telegraf/plugins/inputs/system"
-)
